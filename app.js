cef063a4d9aab7a94ed6176890e202096eeda3cf
Bruno Reis <bruno.nascimento@grupofasitec.com.br>
2023-05-22 17:41:42 -0300

    updated

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const rotaLivros = require("./routes/livro")
+var cors = require('cors')
+
 const app = express()
 app.use(express.json())
+app.use(cors({origin: "*"}))
+
+
 app.use('/livros', rotaLivros)
 
 const port = 8000;
