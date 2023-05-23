8cd99893b4a8a970e6795009ddea5a763e7ac1f8
Bruno Reis <b.reisnascimento@gmail.com>
2023-05-22 23:24:40 -0300

    curso concluído

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const express = require("express")
-const rotaLivros = require("./routes/livro")
-var cors = require('cors')
+const rotaLivro = require("./routes/livro")
+const rotaFavorito = require("./routes/favorito")
+
+const cors = require("cors")
 
 const app = express()
 app.use(express.json())
 app.use(cors({origin: "*"}))
 
+app.use('/livros', rotaLivro)
+app.use('/favoritos', rotaFavorito)
 
-app.use('/livros', rotaLivros)
-
-const port = 8000;
+const port = 8000
 
 app.listen(port, () => {
-    console.log('====================================');
-    console.log(`Escutando a porta ${port}`);
-    console.log('====================================');
+    console.log(`Escutando a porta ${port}`)
 })
-
-
-
